039fbd57980913bdabfa49a38f36b4e70cb6771c
Marcial <nomaspecesfritos@hotmail.com>
2021-02-25 17:37:52 +0100

    responsive navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,22 @@
-import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import HomeIcon from '@material-ui/icons/Home';
 import AboutMeIcon from '@material-ui/icons/Person';
 import ProjectsIcon from '@material-ui/icons/Archive';
 import AptitudesIcon from '@material-ui/icons/Book';
-import MenuIcon from '@material-ui/icons/Menu';
-import Collapse from '@material-ui/core/Collapse';
-import { Button } from '@material-ui/core';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  makeStyles,
+  Button,
+  IconButton,
+  Drawer,
+  Link,
+  MenuItem,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import React, { useState, useEffect } from "react";
+import { Link as RouterLink } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   toolbar: {
@@ -44,6 +51,33 @@ const useStyles = makeStyles((theme) => ({
     stroke: theme.palette.divider,
     strokeWidth: 1,
   },
+  header: {
+    backgroundColor: "#400CCC",
+    paddingRight: "79px",
+    paddingLeft: "118px",
+    "@media (max-width: 900px)": {
+      paddingLeft: 0,
+    },
+  },
+  logo: {
+    fontFamily: "Work Sans, sans-serif",
+    fontWeight: 600,
+    color: "#FFFEFE",
+    textAlign: "left",
+  },
+  menuButton: {
+    fontFamily: "Open Sans, sans-serif",
+    fontWeight: 700,
+    size: "18px",
+    marginLeft: "38px",
+  },
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-around",
+  },
+  drawerContainer: {
+    padding: "20px 30px",
+  },
 }));
 
 const sections = [
@@ -55,6 +89,85 @@ const sections = [
 
 export default function Header(props) {
 
+  const { toolbar, drawerContainer } = useStyles();
+
+  const [state, setState] = useState({
+    mobileView: false,
+    drawerOpen: false,
+  });
+
+  const { mobileView, drawerOpen } = state;
+
+  useEffect(() => {
+    const setResponsiveness = () => {
+      return window.innerWidth < 900
+        ? setState((prevState) => ({ ...prevState, mobileView: true }))
+        : setState((prevState) => ({ ...prevState, mobileView: false }));
+    };
+
+    setResponsiveness();
+
+    window.addEventListener("resize", () => setResponsiveness());
+  }, []);
+
+  const displayDesktop = () => {
+    return (
+      <Toolbar className={classes.toolbarSecondary}>
+        <div>{sections.map((section) => (     
+          <Button
+            key={section.title}
+            href={section.url}
+            className={classes.toolbarLink}
+            endIcon={section.icon}
+          >
+            <a id="navtxt">{section.title}</a>
+          </Button>    
+        ))}</div>
+      </Toolbar>
+    );
+  };
+
+  const displayMobile = () => {
+    const handleDrawerOpen = () =>
+      setState((prevState) => ({ ...prevState, drawerOpen: true }));
+    const handleDrawerClose = () =>
+      setState((prevState) => ({ ...prevState, drawerOpen: false }));
+
+    return (
+      <Toolbar>
+        <IconButton
+          {...{
+            edge: "start",
+            color: "inherit",
+            "aria-label": "menu",
+            "aria-haspopup": "true",
+            onClick: handleDrawerOpen,
+          }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Drawer
+          {...{
+            anchor: "left",
+            open: drawerOpen,
+            onClose: handleDrawerClose,
+          }}
+        >
+          <div className={drawerContainer}><ul style={{listStyle: "none"}}>{sections.map((section) => (     
+            <li><Button
+              key={section.title}
+              href={section.url}
+              className={classes.toolbarLink}
+              startIcon={section.icon}
+            >
+              <a id="navtxt">{section.title}</a>
+            </Button></li> 
+            ))}</ul> 
+          </div>
+        </Drawer> 
+      </Toolbar>
+    );
+  };
   const classes = useStyles();
   return (
     <React.Fragment>
@@ -71,16 +184,7 @@ export default function Header(props) {
         </Typography>
       </Toolbar>
       <Toolbar component="nav"  className={classes.toolbarSecondary}>
-        {sections.map((section) => (     
-          <Button
-            key={section.title}
-            href={section.url}
-            className={classes.toolbarLink}
-            endIcon={section.icon}
-          >
-            <a id="navtxt">{section.title}</a>
-          </Button>    
-        ))}
+      {mobileView ? displayMobile() : displayDesktop()}
       </Toolbar>
     </React.Fragment>
   );
@@ -90,3 +194,5 @@ Header.propTypes = {
   sections: PropTypes.array,
   title: PropTypes.string,
 };
+
+
